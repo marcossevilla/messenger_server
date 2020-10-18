acadd99af4bd62e86e9ce0ecebba78eabc5152c6
Marcos Sevilla <marcossevilla14@gmail.com>
2020-10-17 22:28:18 -0600

    encrypt passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,11 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 
 const createUser = async (req, res = response) => {
 
-    const { email } = req.body;
+    const { email, password } = req.body;
 
     try {
         const emailExists = await User.findOne({ email });
@@ -17,6 +18,11 @@ const createUser = async (req, res = response) => {
         }
 
         const user = new User(req.body);
+
+        // encrypt password
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(password, salt);
+
         await user.save();
 
         res.json({ ok: true, msg: user });
