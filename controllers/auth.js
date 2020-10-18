84e122775fd4de45658fe7864145a8f9d1d3733a
Marcos Sevilla <marcossevilla14@gmail.com>
2020-10-17 20:43:48 -0600

    custom middleware and required fields

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const createUser = (req, res = response) => {
 
--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,23 @@
 */
 
 const { Router, response } = require('express');
+const { check } = require('express-validator');
+
 const { createUser } = require('../controllers/auth');
+const { validateFields } = require('../middlewares/validate_fields');
 
 const router = Router();
 
-router.post('/new', createUser);
+router.post(
+    '/new',
+    [
+        // middlewares
+        check('name', 'Name is required').not().isEmpty(),
+        check('email', 'Email is required').isEmail(),
+        check('password', 'Password is required').not().isEmpty(),
+        validateFields
+    ],
+    createUser
+);
 
 module.exports = router;
