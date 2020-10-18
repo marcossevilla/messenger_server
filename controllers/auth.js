e2489d3b9349175a1f8a55e960628780572916f7
Marcos Sevilla <marcossevilla14@gmail.com>
2020-10-17 23:18:16 -0600

    user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,6 +40,48 @@ const createUser = async (req, res = response) => {
     }
 }
 
+const login = async (req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+        const userFromDB = await User.findOne({ email });
+
+        if (!userFromDB) {
+            return res.status(404).json({
+                ok: false,
+                message: 'User not found'
+            });
+        }
+
+        const validPassword = bcrypt.compareSync(password, userFromDB.password);
+
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                message: 'Password not valid'
+            });
+        }
+
+        // generate JWT
+        const token = await generateJWT(userFromDB.id);
+
+        res.json({
+            ok: true,
+            user: userFromDB,
+            token
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            ok: false,
+            message: 'Communicate with the admin'
+        });
+    }
+}
+
 module.exports = {
-    createUser
+    createUser,
+    login
 }
--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@
 const { Router, response } = require('express');
 const { check } = require('express-validator');
 
-const { createUser } = require('../controllers/auth');
+const { createUser, login } = require('../controllers/auth');
 const { validateFields } = require('../middlewares/validate_fields');
 
 const router = Router();
@@ -13,13 +13,22 @@ const router = Router();
 router.post(
     '/new',
     [
-        // middlewares
         check('name', 'Name is required').not().isEmpty(),
         check('email', 'Email is required').isEmail(),
         check('password', 'Password is required').not().isEmpty(),
         validateFields
     ],
     createUser
+);
+
+router.post(
+    '/',
+    [
+        check('email', 'Email is required').isEmail(),
+        check('password', 'Password is required').not().isEmpty(),
+        validateFields
+    ],
+    login
 );
 
 module.exports = router;
